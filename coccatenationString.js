07499562e4706267cbdf4d1ddd03e718ce38faa5
zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-17 10:25:11 +0200

    added concatenation example

---
+++ coccatenationString.js
@@ -0,0 +1,14 @@
+const hello = 'Hello'
+const world = 'World'
+
+const greeting = `${hello} ${world}`
+
+console.log(greeting)    // Hello World
+
+
+const name = 'Dima'
+const city = 'Kharkiv'
+
+const newGreeting = `My name is ${name}, I live in ${city}`
+
+console.log(newGreeting)   // My name is Dima, I live in Kharkiv
