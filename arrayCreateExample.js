3274ccefdd7ea22ed6e1652a9b365bb5a5b479cf
zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-19 16:17:52 +0200

    added Array examples

--- arrayCreateExample.js
+++ arrayCreateExample.js
@@ -1,2 +1,8 @@
 const myArray = [1, 2, 3, 'hello', true]
 console.log(myArray)   // [ 1, 2, 3, 'hello', true ]
+
+console.log(myArray[0]) // 1
+console.log(myArray[3]) // 'hello'
+console.log(myArray[4]) // true
+
+console.log(myArray.length) // 5
