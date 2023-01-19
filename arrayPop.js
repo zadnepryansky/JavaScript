84f7a5dbaab9224443b46c495d4c2955199f65c6
zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-19 16:42:51 +0200

    added Array methods

---
+++ arrayPop.js
@@ -0,0 +1,8 @@
+const myArray = [1, 2, 3]
+console.log(myArray)
+
+myArray.pop()
+console.log(myArray)  // [ 1, 2 ]
+
+const removedElement = myArray.pop()  // last element in array -> [2]
+console.log(removedElement)  // 2
---
+++ arrayPush.js
@@ -0,0 +1,9 @@
+const myArray = [1, 2, 3]
+
+console.log(myArray)  // [ 1, 2, 3 ]
+
+myArray.push(4)
+console.log(myArray)  // [ 1, 2, 3, 4 ]
+
+myArray.push(true)
+console.log(myArray)  // [ 1, 2, 3, 4, true ]
