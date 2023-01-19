81b8c966552aaa23c05987862738332ef4298c58
zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-19 11:34:36 +0200

    added Array examples

---
+++ arrayCreateExample.js
@@ -0,0 +1,2 @@
+const myArray = [1, 2, 3, 'hello', true]
+console.log(myArray)   // [ 1, 2, 3, 'hello', true ]
---
+++ arrayCreateExample2.js
@@ -0,0 +1,2 @@
+const myArray2 = new Array(1, 2, 3, 'hello', true)
+console.log(myArray2)  // [ 1, 2, 3, 'hello', true ]
