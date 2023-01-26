7dbf3224bff9e299d01849dc5b63384879c1ef72
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-26 16:40:13 +0200

    added ternary operator examples

--- ternaryOperatorExample2.js
+++ ternaryOperatorExample2.js
@@ -1,6 +1,10 @@
 let value = 11
-console.log(value >=0 ? value : -value) // 11
+console.log(value >=0 
+? value 
+: -value) // 11
 
 value = -5
-const res = value >= 0 ? value : -value
+const res = value >= 0 
+? value 
+: -value
 console.log(res)  // 5
