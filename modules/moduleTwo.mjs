d1fbbb741f7c00b183642c031ee4d9654c0187a0
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-28 13:19:33 +0200

    Practise with export and import with modules

---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,8 @@
+import sumNumbers from './moduleOne.mjs'
+
+const res = sumNumbers(10, 2)
+console.log(res)
+// terminal 12
+
+
+// run dode in terminal -> open terminal -> go to the modules folder -> write 'node moduleTwo.mjs'
