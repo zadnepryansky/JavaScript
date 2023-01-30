9e72b6ebbbe19bddfa010c46d0a3f4df95014667
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-30 09:54:18 +0200

    async/await uddated example

---
+++ asyncAwaitExample.js
@@ -0,0 +1,14 @@
+const getData = async (url) => {
+    const res = await fetch(url)  // waiting -> pending
+    const json = await res.json()  // waiting -> pending
+    return json
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+try {
+    const data = await getData(url)
+    console.log(data)
+} catch (error) {
+    console.log(error.message)
+}
